1bc3fc2ee1e51e1d2d5a383101ff22b070bcc30b
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-06-29 16:01:34 +0200

    deployed

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAujP57YOQe3surqWbzNrYo4QIyJVhn7QQ",
-  authDomain: "disney-cl-7c628.firebaseapp.com",
-  projectId: "disney-cl-7c628",
-  storageBucket: "disney-cl-7c628.appspot.com",
-  messagingSenderId: "664224922075",
-  appId: "1:664224922075:web:37c56c9cc3ffe0f8e08ffe",
-  measurementId: "G-JZ6CB6KFKP",
+  apiKey: "AIzaSyC49iR_RNWHIzUpV1bbRTum5fLPClyJJ6w",
+  authDomain: "kejsar-app.firebaseapp.com",
+  projectId: "kejsar-app",
+  storageBucket: "kejsar-app.appspot.com",
+  messagingSenderId: "330860834112",
+  appId: "1:330860834112:web:9fd578a59035e06fe75ea5",
+  measurementId: "G-LVD7MEXG3S",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
